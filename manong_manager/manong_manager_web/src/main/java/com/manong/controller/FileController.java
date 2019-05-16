3a426d8b3ae448525c11f3e8738a64335a54ba8c
dejavu111 <992184445@qq.com>
2019-05-16 19:08:46 +0800

    文件上传下载接口

---
+++ manong_manager/manong_manager_web/src/main/java/com/manong/controller/FileController.java
@@ -0,0 +1,31 @@
+package com.manong.controller;
+
+import com.manong.fastdfs.FastDFSClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * @author ：dejavu111
+ * @date ：Created in 2019/5/16 11:06
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@Controller
+public class FileController {
+    @RequestMapping(value = "uploadfile", method = RequestMethod.POST)
+    @ResponseBody
+    public String fileupload(@RequestParam MultipartFile uploadfile) throws IOException {
+        String fileID = FastDFSClient.uploadFile(uploadfile.getInputStream(),uploadfile.getOriginalFilename());
+        if(fileID != null) {
+            System.out.println("上传文件成功");
+        }
+        return fileID + "------" + uploadfile.getOriginalFilename();
+    }
+}
